e40c01d239c0773778da78ce891981f1f2e45c3a
David Ogor <doctown@yahoo.com>
2016-02-06 10:14:13 -0500

    Recursive numText implemented for one digit

--- numToText.js
+++ numToText.js
@@ -7,5 +7,26 @@ Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and si
 */
 
 var numToText = function(str) {
+  if (0 === str.length) {
+  	return "";
+  }
 
+  if (isNaN(parseInt(str.charAt(0)))) {
+  	return str.charAt(0) + numToText(str.slice(1));
+  } else {
+    return stringConverter[parseInt(str.charAt(0))] + numToText(str.slice(1));
+  }
+};
+
+var stringConverter = {
+  0 : "zero",
+  1 : "one",
+  2 : "two",
+  3 : "three",
+  4 : "four",
+  5 : "five",
+  6 : "six",
+  7 : "seven",
+  8 : "eight",
+  9 : "nine"
 };
